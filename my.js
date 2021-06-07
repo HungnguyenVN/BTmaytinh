29e1be18ac78612c9a75695123a519cfb48b0728
HungnguyenVN <hungnguyen.cnn@gmail.com>
2021-06-07 21:56:30 +0700

    cal

--- my.js
+++ my.js
@@ -1,12 +1,22 @@
-function setOutput(number) {
-    document.getElementById('output').value += number;
+let check = false;
+function setOutput(number){
+    if (check == true){
+        document.getElementById('output').value ="";
+    }
+document.getElementById('output').value += number;
+check = false;
 }
-
-function cal() {
-    let input =  document.getElementById('output').value;
-
-    //ham eval() tinh toan bieu thuc string
-    result = eval(input);
-
-    document.getElementById('output').value = result;
+function cal(){
+let input = document.getElementById('output').value;
+result = eval(input);
+document.getElementById('output').value = result;
+check =true;
+}
+function xoa(){
+    var str = document.getElementById('output').value;
+    var result1 = str.substring(0, str.length-1);
+    document.getElementById('output').value = result1;
+}
+function clear1(){
+    document.getElementById("output").value = "";
 }
